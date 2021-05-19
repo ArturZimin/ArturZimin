f809937e26f8eeba24d411bce060b34a82efb73a
ArturZimin <84173797+ArturZimin@users.noreply.github.com>
2021-05-19 17:14:12 +0300

    Task2

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String hel = new String(" \"Hello World!\"");
         System.out.println(hel);
         String as = new String("\"qqqqwertyuiopasdfghjklzxcvbnm\"");
@@ -48,18 +53,52 @@ public class Main {
 
         System.out.println(args);
 
-       // System.out.println(args[0]);
+        // System.out.println(args[0]);
 
         System.out.println(args[1]);
 
 
-        int wr =1*9;
+        int wr = 1 * 9;
+
+        System.out.println(wr);
+
+        int s = 370000;
+
+        int sec = s % 60;
+
+        int m = (s - sec) / 60;
+        int min = m % 60;
+        int hour = (m - min) / 60;
+
+
+        System.out.println(hour + " часов " + min + " минут " + sec + " cекунд ");
+
 
         //+-*/ %-остаток от деления (5 орифметических действий)
         //for,while,do while,switch case,foreach (для каждого элемента массива,будет что-то делаться)
 
-       // for (int gh=0;gh<args.length; i++) {
-          //  System.out.println(gh[0]);
-        }
+        // for (int gh=0;gh<args.length; i++) {
+        //  System.out.println(gh[0]);
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Type your name");
+        String name = reader.readLine();
+        System.out.println("Hello " + name);
+
+        //Double example
+        System.out.println(1 - 0.42);
+        System.out.println(BigDecimal.valueOf(1).subtract(BigDecimal.valueOf(0.42)));
+
+
+        int mlSec = 5800000;
+        int sec1 = 5800000;
+        int min1 = sec1 / 60;
+        int hours1 = min1 / 60;
+        int twFoHours = hours1 / 24;
+        int week = twFoHours / 7;
+        System.out.println(sec1 + " sec ," + min1 + " min ," + twFoHours + " one day " +
+                " weeks " + week);
     }
+}
 
